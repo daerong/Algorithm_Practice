d260c160b9768defc011f52f39d7fd0f549e908c
daerong <yu5501@nate.com>
2020-07-08 16:12:26 +0900

    Failed - (x, y-1) + (x -1, y) 점화식 사용, 효율성 failed

--- programmers/Daeseong/programmers_42898_U.cpp
+++ programmers/Daeseong/programmers_42898_U.cpp
@@ -4,15 +4,10 @@
 #define MAP_MAX 102
 
 using namespace std;
-
-bool visited[MAP_MAX][MAP_MAX];
+bool wet[MAP_MAX][MAP_MAX];
 int map[MAP_MAX][MAP_MAX];
 int N;
 int M;
-int dx[] = { 0, 1, 0, -1 };
-int dy[] = { -1, 0, 1, 0 };
-int last_step;
-int result;
 
 void setting(int m, int n, vector<vector<int>> puddles) {
     N = n;
@@ -21,46 +16,29 @@ void setting(int m, int n, vector<vector<int>> puddles) {
     while (!puddles.empty()) {
         temp = puddles.back();
         puddles.pop_back();
-        visited[temp[1]][temp[0]] = true;
+        wet[temp[1]][temp[0]] = true;
     }
-    for (int y = 1; y <= N; y++) {
-        for (int x = 1; x <= M; x++) {
-            map[y][x] = 1000000000;
-        }
+
+    map[1][1] = 1;
+    for (int x = 2; x <= M; x++) {
+        if (wet[1][x]) continue;
+        map[1][x] = map[1][x - 1];
+    }
+    for (int y = 2; y <= N; y++) {
+        if (wet[y][1]) continue;
+        map[y][1] = map[y - 1][1];
     }
 }
-void connection(int x, int y, int cnt) {
-    if (visited[y][x]) return;
-    if (x <= 0 || x > M || y <= 0 || y > N) return;
-    if (cnt > map[y][x]) return;
-
-    if (x == M && y == N) {
-        if (map[y][x] > cnt) {
-            map[y][x] = cnt;
-            result = 1;
+void connection() {
+    for (int y = 2; y <= N; y++) {
+        for (int x = 2; x <= M; x++) {
+            if (wet[y][x]) continue;
+            map[y][x] = map[y - 1][x] + map[y][x - 1];
         }
-        else {
-            result += 1;
-        }
-
-        return;
     }
-
-    visited[y][x] = true;
-    map[y][x] = cnt;
-
-    int nx, ny;
-    for (int dir = 0; dir < 4; dir++) {
-        nx = x + dx[dir];
-        ny = y + dy[dir];
-        connection(nx, ny, cnt + 1);
-    }
-
-    visited[y][x] = false;
 }
 int solution(int m, int n, vector<vector<int>> puddles) {
     setting(m, n, puddles);
-    connection(1, 1, 0);
-    int answer = result;
-    return answer;
+    connection();
+    return map[N][M] % 1000000007;
 }
