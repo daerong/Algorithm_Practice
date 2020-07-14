6f1bd79527e2cb78c5527321877e8e932a4ca38c
daerong <yu5501@nate.com>
2020-07-15 00:21:36 +0900

    Solution - DP, O(n)

--- programmers/Daeseong/programmers_42897_U.cpp
+++ programmers/Daeseong/programmers_42897_U.cpp
@@ -1,53 +1,32 @@
-// start : pm 9:53
-// end :
-// try : 
-
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <iostream>
 
 using namespace std;
 
-#define N_MAX 1000002
-
-bool visited[N_MAX];
-int N;
-int result;
+int arr1[1000000];
+int arr2[1000000];
 
 int solution(vector<int> money) {
     int answer = 0;
-    // 초기화섹션
-    // money_sum 초기화 필요시 작성
-    // 초기화섹션
 
-    priority_queue<pair<int, int>> pq;
-    N = money.size();
-    for (int n = 1; n <= N; n++) {
-        pq.push({ money[n - 1], n });
+    int N = money.size();
+
+    arr1[0] = money[0];
+    arr2[0] = 0;
+    arr1[1] = money[1];
+    arr2[1] = money[1];
+    arr1[2] = money[2] + arr1[0];
+    arr2[2] = money[2] + arr2[0];
+
+    for (int i = 3; i < N; i++) {
+        arr1[i] = money[i] + max(arr1[i - 2], arr1[i - 3]);
+        arr2[i] = money[i] + max(arr2[i - 2], arr2[i - 3]);
     }
 
-    pair<int, int> temp;
-    while (!pq.empty()) {
-        temp = pq.top();
-        pq.pop();
+    arr1[N - 1] -= money[N - 1];
 
-        if (visited[temp.second - 1] || visited[temp.second + 1]) continue;
-        
-        visited[temp.second] = true;
-        answer += temp.first;
-    }
-    
+    answer = max(max(arr1[N - 2], arr1[N - 1]), max(arr2[N - 2], arr2[N - 1]));
+
     return answer;
-}
-
-int main() {
-    vector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(1);
-    cout << solution(vec);
-    return 0;
 }
