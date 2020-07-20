2f8314cd191e42cad61cc123652099a3a639387b
daerong <yu5501@nate.com>
2020-07-20 22:59:36 +0900

    Solution - The magic square has 8 shapes. - So we find the diffrence between 8 cases and given one. - If you print the smallest of 8 cases, you can pass this problem.

--- hackerrank/Daeseong/forming_a_magic_square_U.cpp
+++ hackerrank/Daeseong/forming_a_magic_square_U.cpp
@@ -1,108 +1,58 @@
+// The magic square has 8 shapes.
+// So we find the diffrence between 8 cases and given one.
+// If you print the smallest of 8 cases, you can pass this problem.
+
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
-#define MID 1
-#define MATRIX 3
-#define NUM 10
+int magic_squares[8][3][3] = {
+    {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+    {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+    {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+    {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+    {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+    {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+    {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+    {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}
+};
 
-int map[MATRIX][MATRIX];
-int num[NUM];
-// U ~ clock dir
-int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+int arr[3][3];
 
 void input() {
-	for (int y = 0; y < MATRIX; y++) {
-		for (int x = 0; x < MATRIX; x++) {
-			cin >> map[y][x];
-		}
-	}
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 3; x++) {
+            cin >> arr[y][x];
+        }
+    }
 }
 
-bool is_ten(int a, int b) {
-	if (a + b == 10) return true;
-	else return false;
-}
-
-int solution() {
-	int result = 0;
-	if (map[MID][MID] != 5) {
-		result += abs(map[MID][MID] - 5);
-	}
-
-	for (int y = 0; y < MATRIX; y++) {
-		for (int x = 0; x < MATRIX; x++) {
-			num[map[y][x]] += 1;
-		}
-	}
-
-	for (int d = 0; d < 4; d++) {
-		int a = map[MID + dy[d]][MID + dx[d]];
-		int b = map[MID + dy[d + 4]][MID + dx[d + 4]];
-		if (is_ten(a, b)) continue;
-
-		if (num[a] != 1 && num[b] != 1) {
-			if (num[NUM - a] == 0) {
-				result += abs(map[MID + dy[d + 4]][MID + dx[d + 4]] - (NUM - a));
-				map[MID + dy[d + 4]][MID + dx[d + 4]] = NUM - a;
-			}
-			else if (num[NUM - b] == 0) {
-				result += abs(map[MID + dy[d]][MID + dx[d]] - (NUM - b));
-				map[MID + dy[d]][MID + dx[d]] = NUM - b;
-			}
-		}
-		else if (num[a] != 1) {
-			num[a] = num[a] - 1;
-			int temp = NUM - b;
-			result += abs(a - temp);
-			map[MID + dy[d]][MID + dx[d]] = temp;
-			num[temp] = num[b] + 1;
-		}
-		else if (num[b] != 1) {
-			num[b] = num[b] - 1;
-			int temp = NUM - a;
-			result += abs(b - temp);
-			map[MID + dy[d + 4]][MID + dx[d + 4]] = temp;
-			num[temp] = num[b] + 1;
-		}
-		else {
-			if (num[NUM - a] == 0) {
-				result += abs(map[MID + dy[d + 4]][MID + dx[d + 4]] - (NUM - a));
-				map[MID + dy[d + 4]][MID + dx[d + 4]] = NUM - a;
-			}
-			else if (num[NUM - b] == 0) {
-				result += abs(map[MID + dy[d]][MID + dx[d]] - (NUM - b));
-				map[MID + dy[d]][MID + dx[d]] = NUM - b;
-			}
-		}
-	}
-
-	return result;
-}
-
-void show() {
-	for (int y = 0; y < MATRIX; y++) {
-		for (int x = 0; x < MATRIX; x++) {
-			cout << map[y][x];
-		}
-		cout << endl;
-	}
+int solution(int n) {
+    int sum = 0;
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 3; x++) {
+            sum += abs(magic_squares[n][y][x] - arr[y][x]);
+        }
+    }
+    return sum;
 }
 
 void solve() {
-	input();
-	cout << solution() << endl;
-	//show();
+    input();
+    int result = 1000000000;
+    for (int n = 0; n < 8; n++) {
+        result = min(result, solution(n));
+    }
+    cout << result;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	solve();
+    solve();
 
-	return 0;
+    return 0;
 }
