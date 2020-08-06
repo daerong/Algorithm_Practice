5007dfc0dbadbc91da8aeb3b1a2b04d11ad0a729
daerong <yu5501@nate.com>
2020-08-06 21:00:37 +0900

    Solution - 다음의 방식을 이용하면 나가는 차를 모두 측정할 수 있다. - 1. 차의 진입순서를 빠른 순서로 정렬한다. - 2. 처음 차의 진출 시기를 end_point에 저장한다. - 3. 다음 차의 진출 시기가 end_point보다 작은 경우, end_point를 작은 값으로 변경한다. - 4. 다음 차의 진입 시기가 end_point보다 작은 경우, 결과 값을 1 증가시킨다.  - 이 경우 이전의 차량은 모두 측정이 완료되었으므로 end_point를 새로운 차량의 진출 시기로 변경한다. -5. 마지막 반복에 대해서는 종료시기를 정하지 못했으므로 결과 값을 1 증가시킨다.

---
+++ programmers/Daeseong/programmers_42884_U.java
@@ -0,0 +1,34 @@
+//Solution
+//    - 다음의 방식을 이용하면 나가는 차를 모두 측정할 수 있다.
+//    - 1. 차의 진입순서를 빠른 순서로 정렬한다.
+//    - 2. 처음 차의 진출 시기를 end_point에 저장한다.
+//    - 3. 다음 차의 진출 시기가 end_point보다 작은 경우, end_point를 작은 값으로 변경한다.
+//    - 4. 다음 차의 진입 시기가 end_point보다 작은 경우, 결과 값을 1 증가시킨다.
+//        - 이 경우 이전의 차량은 모두 측정이 완료되었으므로 end_point를 새로운 차량의 진출 시기로 변경한다.
+//    -5. 마지막 반복에 대해서는 종료시기를 정하지 못했으므로 결과 값을 1 증가시킨다.
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+        int end_point = routes[0][1];
+
+        for (int i = 1; i < routes.length; i++) {
+            if (routes[i][1] < end_point) {
+                end_point = routes[i][1];
+                continue;
+            }
+
+            if (routes[i][0] > end_point) {
+                answer += 1;
+                end_point = routes[i][1];
+            }
+        }
+
+        answer += 1;
+
+        return answer;
+    }
+}
