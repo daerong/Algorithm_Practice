798ee66e738f86dea9773a99feadfe9a55252ab0
Youngho Jo <dodghek1@naver.com>
2020-08-09 07:02:44 +0900

    Update Target_Number.java

--- hackerrank/Youngho/dfs/Target_Number.java
+++ hackerrank/Youngho/dfs/Target_Number.java
@@ -1,23 +1,21 @@
 //link : https://programmers.co.kr/learn/courses/30/lessons/43165?language=java
+//난이도 : medium hard
 
 class Solution {
     public int solution(int[] numbers, int target) {
         int answer = 0;
         answer = dfs(numbers, 0, 0, target);
         return answer;
+    }
         
-        private int dfs(int[] arr, int len, int sum, int target){
+    private int dfs(int[] arr, int len, int sum, int target){
             if(len == arr.length){
-                if(sum == target){
+                if(sum == target)
                     return 1;
-                }else{
-                    return 0;
-                }
+                return 0;
             }
-            dfs(arr[i], len++);
-            dfs(arr[i]*-1, len++);            
-         
-        
+            return dfs(arr, len+1, sum + arr[len], target) + 
+                   dfs(arr, len+1, sum + arr[len]*-1, target);         
     }
 }
 
