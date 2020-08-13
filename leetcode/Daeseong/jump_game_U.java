bda34aba21552d41799c2f2ea066e0e630d4b79c
daerong <yu5501@nate.com>
2020-08-13 23:00:29 +0900

    Solution - Using DFS. - should to do visit check. If you don't, it should be occur time out error.

--- leetcode/Daeseong/jump_game_U.java
+++ leetcode/Daeseong/jump_game_U.java
@@ -1,10 +1,20 @@
+//Solution
+//- Using DFS.
+//- should to do visit check. If you don't, it should be occur time out error.
+
+
+import java.util.Arrays;
+
 class Solution {
     int last;
     int[] nums_copy;
+    boolean[] visited;
     boolean result = false;
 
     public boolean canJump(int[] nums) {
         nums_copy = new int[nums.length];
+        visited = new boolean[nums.length];
+        Arrays.fill(visited, false);
         System.arraycopy(nums, 0, nums_copy, 0, nums.length);
 
         last = nums.length - 1;
@@ -18,6 +28,8 @@ class Solution {
             return;
         }
         if(step > last) return;
+        if(visited[step]) return;
+        visited[step] = true;
 
         for(int i = 1; i <= nums_copy[step]; i++){
             dfs(step + i);
