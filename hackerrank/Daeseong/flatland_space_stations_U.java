005ae7240122c95f2c355a0b912d130463ef02cb
daerong <yu5501@nate.com>
2020-08-29 23:53:06 +0900

    Failed - Can't test. - Server connection error.

---
+++ hackerrank/Daeseong/flatland_space_stations_U.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the flatlandSpaceStations function below.
+    static int flatlandSpaceStations(int n, int[] c) {
+        int m = c.length;
+        if(n == m) return 0;
+        Arrays.sort(c);
+
+        int result = Math.max(c[0], n - 1 - c[m - 1]);
+        for(int i = 1; i < c.length; i++){
+            result = Math.max(result, (c[i] - c[i - 1]) / 2);
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[] c = new int[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = flatlandSpaceStations(n, c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
