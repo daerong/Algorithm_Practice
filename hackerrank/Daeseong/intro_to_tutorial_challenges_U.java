f354833cd2ff90b19a762a40cb020caffec63694
daerong <yu5501@nate.com>
2020-09-14 11:31:37 +0900

    Solution - Easy to solve using the Binary Search.

---
+++ hackerrank/Daeseong/intro_to_tutorial_challenges_U.java
@@ -0,0 +1,61 @@
+//Solution
+//- Easy to solve using the Binary Search.
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the introTutorial function below.
+    static int introTutorial(int V, int[] arr) {
+        int start = 0;
+        int end = arr.length;
+        while (start <= end){
+            int mid = (end + start) / 2;
+            if (arr[mid] < V) start = mid + 1;
+            else if(arr[mid] > V) end = mid - 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = introTutorial(V, arr);
+
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
