fd8ceb93764ae2bcb6795196b8508d24033eae50
daerong <yu5501@nate.com>
2020-09-21 22:33:30 +0900

    Solution - sort를 이해하고 있다면 쉽게 풀수있다.

---
+++ hackerrank/Daeseong/correctness_and_the_loop_Invariant_U.java
@@ -0,0 +1,29 @@
+//Solution
+//- sort를 이해하고 있다면 쉽게 풀수있다.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void insertionSort(int[] A){
+        Arrays.sort(A);
+        printArray(A);
+    }
+
+
+    static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+            ar[i]=in.nextInt();
+        }
+        insertionSort(ar);
+    }
+}
