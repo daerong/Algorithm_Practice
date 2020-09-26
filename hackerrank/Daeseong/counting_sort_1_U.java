9a727be76ed825c13651622ef23f887a4bbb6d04
daerong <yu5501@nate.com>
2020-09-26 22:26:34 +0900

    Solution - 비교가 아닌 counting을 통해 정렬하는 방식 - 0~99까지의 숫자의 갯수를 각각세고 각 숫자의 갯수를 리턴한다.

---
+++ hackerrank/Daeseong/counting_sort_1_U.java
@@ -0,0 +1,58 @@
+//Solution
+//- 비교가 아닌 counting을 통해 정렬하는 방식
+//- 0~99까지의 숫자의 갯수를 각각세고 각 숫자의 갯수를 리턴한다.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingSort function below.
+    static int[] countingSort(int[] arr) {
+        int[] counts = new int[100];
+        Arrays.fill(counts, 0);
+        for(int i = 0; i < arr.length; i++) counts[arr[i]] += 1;
+
+        return counts;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = countingSort(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
