5b1e9f221e9ab0b959fdbaec38f68748da3a9fe3
daerong <yu5501@nate.com>
2020-10-06 23:40:57 +0900

    Failed - Url 파싱 방식을 구현하는 방식일 것 같음. - 기본점수는 명확한데 예제에서 외부 링크 점수가 왜 1, 2, 3인지 모르겠음.

---
+++ programmers/Daeseong/programmers_42983_U.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// Code start
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string word, vector<string> pages) {
+    int answer = 0;
+    return answer;
+}
+
+// Code end
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string word = "blind";
+    vector<string> pages;
+    pages.push_back("<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>");
+    pages.push_back("<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>");;
+    pages.push_back("<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>");
+
+    cout << solution(word, pages);
+
+    return 0;
+}
