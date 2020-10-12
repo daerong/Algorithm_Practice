3cc8ab98a7bac5365092efeeae1bbb51e67cbe58
daerong <yu5501@nate.com>
2020-10-12 23:32:45 +0900

    Failed - 시간초과, 성능개선 필요.

--- baekjoon/Daeseong/16235_U.cpp
+++ baekjoon/Daeseong/16235_U.cpp
@@ -1,41 +1,117 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int result;
-int N;
-int **matrix;
+#define endl '\n'
+#define N_MAX 10 + 1
 
-void input() {
-	cin >> N;
-	matrix = new int*[N];
-	for (int y = 0; y < N; y++) {
-		matrix[y] = new int[N];
-		for(int x = 0; x < N; x++){
-			cin >> matrix[y][x];
+typedef struct TREE {
+	int x;
+	int y;
+	int year;
+} TREE;
+
+bool compare(TREE a, TREE b) {
+	return a.year < b.year;
+}
+
+int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+
+int N;		// 맵 크기
+int M;		// 나무 수
+int K;		// term
+int soil[N_MAX][N_MAX];
+int A[N_MAX][N_MAX];
+
+queue<TREE> spring(vector<TREE>& trees) {
+	queue<TREE> dead_tree;
+
+	for (vector<TREE>::iterator iter = trees.begin(); iter != trees.end(); ) {
+		if (soil[(*iter).x][(*iter).y] >= (*iter).year) {
+			soil[(*iter).x][(*iter).y] -= (*iter).year;
+			(*iter).year += 1;
+
+			iter++;
+		}
+		else {
+			dead_tree.push(*iter);
+			iter = trees.erase(iter);
+		}
+	}
+
+	return dead_tree;
+}
+
+void summer(queue<TREE> dead_tree) {
+	TREE temp;
+	while (!dead_tree.empty()) {
+		temp = dead_tree.front();
+		dead_tree.pop();
+
+		soil[temp.x][temp.y] += temp.year / 2;
+	}
+}
+
+void fall(vector<TREE>& trees) {
+	int tree_vol = trees.size();
+	for (int i = 0; i < tree_vol; i++) {
+		if (trees[i].year % 5 == 0) {
+			for (int dir = 0; dir < 8; dir++) {
+				int nx = trees[i].x + dx[dir];
+				int ny = trees[i].y + dy[dir];
+				if (nx <= 0 || nx > N || ny <= 0 || ny > N) continue;
+				trees.push_back({ nx, ny, 1 });
+			}
 		}
 	}
 }
 
-void solution() {
-	int upperSum = 0;
-	int lowerSum = 0;
-	for (int i = 0; i < N; i++) {
-		upperSum += matrix[i][N - 1 - i];
-		lowerSum += matrix[i][i];
+void winter() {
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			soil[r][c] += A[r][c];
+		}
 	}
-
-	result = abs(upperSum - lowerSum);
 }
 
-void solve() {
-	input();
-	solution();
-	cout << result;
+int solution(vector<TREE> trees, int K) {
+	for (int k = 1; k <= K; k++) {
+		summer(spring(trees));
+		fall(trees);
+		winter();
+	}
+
+	return trees.size();
 }
 
 int main() {
-	solve();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<TREE> trees;
+
+	cin >> N >> M >> K;
+
+	for (int r = 1; r <= N; r++) {
+		for (int c = 1; c <= N; c++) {
+			cin >> A[r][c];
+			soil[r][c] = 5;
+		}
+	}
+
+	int x, y, z;
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y >> z;
+		trees.push_back({ x, y, z });
+	}
+
+	cout << solution(trees, K);
+
 	return 0;
 }
