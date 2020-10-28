40038d4d03e6912f1dc58fe145c10f05e69ad57b
daerong <yu5501@nate.com>
2020-10-29 00:08:04 +0900

    Solution - 1번 문자열에서 2번 문자열을 만들기 위해 삭제하거나 추가해야할 알파벳 갯수를 구하는 문제 - LCS 인줄 알고 풀었으나 알파벳 갯수비교로 구할 수 있었다. - 1. 1번 문자열의 각 알파벳의 갯수를 구한다. - 2. 2번 문자열의 각 알파벳의 갯수를 구한다. - 3. 1.과 2.에서 구한 각 알파벳 숫자의 차이 값의 합을 구한다.

--- hackerrank/Daeseong/making_anagrams_U.java
+++ hackerrank/Daeseong/making_anagrams_U.java
@@ -1,3 +1,10 @@
+//Solution
+//- 1번 문자열에서 2번 문자열을 만들기 위해 삭제하거나 추가해야할 알파벳 갯수를 구하는 문제
+//- LCS 인줄 알고 풀었으나 알파벳 갯수비교로 구할 수 있었다.
+//- 1. 1번 문자열의 각 알파벳의 갯수를 구한다.
+//- 2. 2번 문자열의 각 알파벳의 갯수를 구한다.
+//- 3. 1.과 2.에서 구한 각 알파벳 숫자의 차이 값의 합을 구한다.
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -10,15 +17,23 @@ public class Solution {
 
     // Complete the makingAnagrams function below.
     static int makingAnagrams(String s1, String s2) {
-        int answer = 0;
+        int[] alphabet = new int[26];
+        Arrays.fill(alphabet, 0);
 
-
-        for(int i = 0; i < s1.length(); i++){
-            int cnt = 0;
-            for(int j = 0; j < s2.length(); j++){
-
-            }
+        for(int i = 0; i < s1.length(); i++) {
+            alphabet[s1.charAt(i) - 'a'] += 1;
         }
+
+        for(int j = 0; j < s2.length(); j++) {
+            alphabet[s2.charAt(j) - 'a'] -= 1;
+        }
+
+        int answer = 0;
+        for(int k = 0; k < 26; k++){
+            answer += Math.abs(alphabet[k]);
+        }
+
+        return answer;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
