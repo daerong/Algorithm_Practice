af92faae96471820351b3fcc860cce53b3dd63c6
daerong <yu5501@nate.com>
2020-10-28 23:50:57 +0900

    Upload problem.

---
+++ hackerrank/Daeseong/making_anagrams_U.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the makingAnagrams function below.
+    static int makingAnagrams(String s1, String s2) {
+        int answer = 0;
+
+
+        for(int i = 0; i < s1.length(); i++){
+            int cnt = 0;
+            for(int j = 0; j < s2.length(); j++){
+
+            }
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = scanner.nextLine();
+
+        String s2 = scanner.nextLine();
+
+        int result = makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
