7640149790aeae2a787221f76f3e34c4c14e0eaa
daerong <yu5501@nate.com>
2020-12-04 22:41:06 +0900

    Upload next problem.

---
+++ baekjoon/Daeseong/19237_U.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void init() {
+
+}
+
+void solve() {
+    init();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
