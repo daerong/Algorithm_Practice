96d440e0795267cb78bcc7a53ccc6f8391c44825
daerong <yu5501@nate.com>
2021-01-05 23:41:42 +0900

    Failed - 테케 2번 실패 - 실행시간 0.01ms로 예외처리가 안되있는 걸로 보임

--- programmers/Daeseong/programmers_42895_U.cpp
+++ programmers/Daeseong/programmers_42895_U.cpp
@@ -1,45 +1,53 @@
 #include <string>
-#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 #define CNT_MAX 8
 
 using namespace std;
 
-vector<int> uds[CNT_MAX + 1];
+int solution(int N, int number) {
+    if (N == number) return 1;
+    if (number <= 1) return 2;
 
-int expand(int number) {
-    for (int i = 1; i <= CNT_MAX; i++) {
-        int J = uds[i].size();
-        for (int j = 0; j < J; j++) {
-            if (uds[i][j] == number) return i;
-            for (int ii = i + 1; ii + i <= CNT_MAX; ii++) {
-                int JJ = uds[ii].size();
-                for (int jj = 0; jj < JJ; jj++) {
-                    uds[ii + i].push_back(uds[i][j] + uds[ii][jj]);
-                    if (abs(uds[i][j] - uds[ii][jj])) uds[ii + i].push_back(abs(uds[i][j] - uds[ii][jj]));
-                    uds[ii + i].push_back(uds[i][j] * uds[ii][jj]);
-                    if (uds[i][j] / uds[ii][jj]) uds[ii + i].push_back(uds[i][j] / uds[ii][jj]);
-                    else uds[ii + i].push_back(uds[ii][jj] / uds[i][j]);
+    unordered_map<int, bool> dp[CNT_MAX + 1];
+    dp[1].insert({ N, true });
+    int temp = N;
+    for (int i = 2; i <= CNT_MAX; i++) {
+        temp = temp * 10 + N;
+        dp[i].insert({ temp, true });
+    }
+
+    for (int cnt = 2; cnt <= 8; cnt++) {
+        for (int num1 = 1; num1 <= cnt / 2; num1++) {
+            int num2 = cnt - num1;
+            for (unordered_map<int, bool>::iterator it1 = dp[num1].begin(); it1 != dp[num1].end(); it1++) {
+                int temp;
+                for (unordered_map<int, bool>::iterator it2 = dp[num2].begin(); it2 != dp[num2].end(); it2++) {
+                    temp = it1->first + it2->first;
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });
+                    temp = abs(it1->first - it2->first);
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });
+                    /*temp = it1->first - it2->first;
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });
+                    temp = it2->first - it1->first;
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });*/
+                    temp = it1->first * it2->first;
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });
+                    if (temp == 0) continue;
+                    if (it1->first >= it2->first) temp = it1->first / it2->first;
+                    else temp = it2->first / it1->first;
+                    if (temp == number) return cnt;
+                    if (dp[cnt].count(temp) == 0) dp[cnt].insert({ temp, true });
                 }
             }
         }
     }
 
     return -1;
-}
-
-int solution(int N, int number) {
-    uds[2].push_back(1);
-    int temp = N;
-    int temp2 = N;
-    uds[1].push_back(temp);
-    for (int i = 2; i <= CNT_MAX; i++) {
-        temp = temp * 10 + N;
-        temp2 = temp2 * N;
-        uds[i].push_back(temp);
-        uds[i].push_back(temp2);
-    }
-
-    int answer = expand(number);
-    return answer;
 }
